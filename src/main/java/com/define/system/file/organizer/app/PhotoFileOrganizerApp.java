df8aff8c1b2dfe32890012acb18a799234c7b300
niteshapte <niteshapte@users.noreply.github.com>
2020-01-04 12:34:26 +0200

    Added license and java doc

--- src/main/java/com/define/system/file/organizer/app/PhotoFileOrganizerApp.java
+++ src/main/java/com/define/system/file/organizer/app/PhotoFileOrganizerApp.java
@@ -9,11 +9,37 @@ import com.define.system.file.organizer.dto.UserInputDTO;
 import com.define.system.file.organizer.processor.PhotoOrganizer;
 import com.define.system.file.organizer.util.PropertiesLoader;
 
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Photo organizer strategy
+ * 
+ * @author Nitesh Apte
+ * @version 0.1
+ * @since 0.1
+ */
 public class PhotoFileOrganizerApp implements IFileOrganizerApp {
 	
 	/** The Constant logger. */
 	final static Logger logger = Logger.getLogger(PhotoFileOrganizerApp.class);
 	
+	/* (non-Javadoc)
+	 * @see com.define.system.file.organizer.app.IFileOrganizerApp#init()
+	 */
 	@Override
 	public void init() {
 		UserInputDTO userInputDTO = collectInput();
@@ -25,6 +51,11 @@ public class PhotoFileOrganizerApp implements IFileOrganizerApp {
 		}
 	}
 	
+	/**
+	 * Collect input from properties file.
+	 *
+	 * @return the user input DTO
+	 */
 	private UserInputDTO collectInput() {
 		PropertiesLoader propertiesLoader = PropertiesLoader.getInstance();
 		propertiesLoader.loadFile(FileOrganizerConstant.INPUT_PROP_FILE_PATH);
--- src/main/java/com/define/system/file/organizer/constants/FileOrganizerConstant.java
+++ src/main/java/com/define/system/file/organizer/constants/FileOrganizerConstant.java
@@ -1,41 +1,83 @@
 package com.define.system.file.organizer.constants;
 
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Constants for project.
+ *
+ * @author Nitesh Apte
+ * @version 0.1
+ * @version 0.1
+ */
 public class FileOrganizerConstant {
 	
+	/**
+	 * Instantiates a new file organizer constant.
+	 */
 	FileOrganizerConstant() { 	}
 
+	/** The Constant INPUT_PROP_FILE_PATH. */
 	public final static String INPUT_PROP_FILE_PATH = "./inputprop/file-organizer.properties";
 	
+	/** The Constant PHOTO_ORGANIZER_ENABLED. */
 	public final static String PHOTO_ORGANIZER_ENABLED = "boolean.file.organize.photo";
 	
+	/** The Constant PHOTO_STRING_SOURCE_LOCATION. */
 	public final static String PHOTO_STRING_SOURCE_LOCATION = "string.photo.source.location";
 	
+	/** The Constant PHOTO_STRING_DESTINATION_LOCATION. */
 	public final static String PHOTO_STRING_DESTINATION_LOCATION = "string.photo.destination.location";
 
+	/** The Constant PHOTO_LIST_FILE_EXTENSION. */
 	public final static String PHOTO_LIST_FILE_EXTENSION = "list.photo.file.extension";
 	
+	/** The Constant PHOTO_BOOLEAN_CREATE_FOLDER. */
 	public final static String PHOTO_BOOLEAN_CREATE_FOLDER = "boolean.photo.create.folder";
 	
 	
+	/** The Constant VIDEO_ORGANIZER_ENABLED. */
 	public final static String VIDEO_ORGANIZER_ENABLED = "boolean.file.organize.video";
 	
+	/** The Constant VIDEO_STRING_SOURCE_LOCATION. */
 	public final static String VIDEO_STRING_SOURCE_LOCATION = "string.video.source.location";
 	
+	/** The Constant VIDEO_STRING_DESTINATION_LOCATION. */
 	public final static String VIDEO_STRING_DESTINATION_LOCATION = "string.video.destination.location";
 
+	/** The Constant VIDEO_LIST_FILE_EXTENSION. */
 	public final static String VIDEO_LIST_FILE_EXTENSION = "list.video.file.extension";
 	
+	/** The Constant VIDEO_BOOLEAN_CREATE_FOLDER. */
 	public final static String VIDEO_BOOLEAN_CREATE_FOLDER = "boolean.video.create.folder";
 	
 	
+	/** The Constant DOC_ORGANIZER_ENABLED. */
 	public final static String DOC_ORGANIZER_ENABLED = "boolean.file.organize.doc";
 	
+	/** The Constant DOC_STRING_SOURCE_LOCATION. */
 	public final static String DOC_STRING_SOURCE_LOCATION = "string.doc.source.location";
 	
+	/** The Constant DOC_STRING_DESTINATION_LOCATION. */
 	public final static String DOC_STRING_DESTINATION_LOCATION = "string.doc.destination.location";
 
+	/** The Constant DOC_LIST_FILE_EXTENSION. */
 	public final static String DOC_LIST_FILE_EXTENSION = "list.doc.file.extension";
 	
+	/** The Constant DOC_BOOLEAN_CREATE_FOLDER. */
 	public final static String DOC_BOOLEAN_CREATE_FOLDER = "boolean.doc.create.folder";
 	
 }
